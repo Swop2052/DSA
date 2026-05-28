1ca36e8b3e0feb85c02111f4ddf49ebb5c2e26e1
Swapnil <ingle8042@gmail.com>
2026-05-28 16:47:52 +0530

    New Program

---
+++ String.py
@@ -0,0 +1,27 @@
+# print string
+text = "Hello World"
+print(text)
+
+# Find length of string
+text = "Hello World"
+print(len(text))
+
+# access character
+text = "Hello World"
+print(text[0])
+
+# String slicing
+text = "Hello World"
+print(text[0:2])
+
+# Convert lowercase
+text = "Hello World"
+print(text.lower())
+
+# Replace Charcter
+text = "Hello World"
+print(text.replace("H","E"))
+
+# split string
+text = "Hello World"
+print(text.split)
