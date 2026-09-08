45fc6b19fc7b873e2ddc5e6f583c6824562f41fa
Swapnil <ingle8042@gmail.com>
2026-09-08 22:16:07 +0530

    update tuple

--- Tuple/Unpacking.py
+++ Tuple/Unpacking.py
@@ -1,7 +1,14 @@
 """
-Topic: Unpacking a Tuple
-Description: "Packing" a tuple is when we assign values to it.
-"Unpacking" is when we extract those values back into separate variables.
+Topic: Packing and Unpacking a Tuple
+
+Definitions:
+1. Packing: When we create a tuple and assign multiple values to it, 
+   it is called "packing" a tuple. We are packing all items into one single variable.
+   Example: student = ("Rahul", 20, "Pune")
+
+2. Unpacking: When we take those values OUT of the tuple and assign them 
+   back into individual/separate variables, it is called "unpacking".
+   Example: (name, age, city) = student
 """
 
 # Packing a tuple with student info
